d51e3f0d6e431997cd2fac0cfaaf2e61260136ef
IamBananana <richardgjelland@hotmail.com>
2023-09-08 14:58:02 +0200

    added js to contact form. added contact, about us and finished up the website.

---
+++ submit.js
@@ -0,0 +1,15 @@
+
+
+var input = document.getElementById("input");
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter"){
+        event.preventDefault();
+        document.getElementById("button").click();
+    }
+})  
+function handle_form_submission()
+{
+  window.location.replace('kontakt-oss.html');
+  alert('Skjemaet er sendt!');
+    return false;
+}
